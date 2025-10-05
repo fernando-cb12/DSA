8a5967f42c30b663eb4cff3e484e95d764ee4edd
fernando-cb12 <fernandocamoub@gmail.com>
2025-10-05 16:25:26 -0600

    first commit

---
+++ LinkedList.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def search(self, target):
+        current = self.head
+        while current:
+            if current.data == target:
+                return current
+            current = current.next
+        return None
+    def insert(self, node):
+        node.next = self.head
+        if self.head:
+            self.head.prev = node
+        self.head = node
+        node.prev = None
+    def delete(self, node):       
+        if node.prev is not None:
+            node.prev.next
+        else:
+            self.head = node.next
+        if node.next is not None:
+            node.next.prev = node.prev
